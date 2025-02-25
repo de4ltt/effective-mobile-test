99d00a7cf6234fcb40fd00e5b9d4e3c95e9775f5
Timko Aleksey <127128699+de4ltt@users.noreply.github.com>
2025-02-25 19:34:05 +0300

    feat: add RetrofitClient

---
+++ network/src/main/java/com/example/network/client/RetrofitClient.kt
@@ -0,0 +1,31 @@
+package com.example.network.client
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+    private const val BASE_URL = "https://work.example.com/"
+
+    private val okHttpClient: OkHttpClient by lazy {
+        val logging = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+        OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .build()
+    }
+
+    private val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    val vacanciesApi: VacanciesApi by lazy {
+        retrofit.create(VacanciesApi::class.java)
+    }
+}
